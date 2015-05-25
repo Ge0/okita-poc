e3c277b3b923db3fc9970735ed6c0f1e007dcfcc
Geoffrey ROYER <geoffrey.royer@gmail.com>
2015-05-25 19:49:06 +0200

    Forgotten the core files

---
+++ okita/binary_disassembler.py
@@ -0,0 +1,78 @@
+import capstone
+import struct
+
+class BinaryDisassembler(object):
+    def set_org(self, org):
+        pass
+    def disassemble_unknown_region(self, region, data):
+        pass
+    def disassemble_code_region(self, region, data):
+        pass
+    def disassemble_data_region(self, region, data):
+        pass
+    def disassemble_elf32_ehdr_region(self, region, data):
+        pass
+
+class NaiveBinaryDisassembler(BinaryDisassembler):
+    def __init__(self, output_file):
+        self._handle = open("%s_naive_disass.asm" % (output_file), "w")
+        self._handle.write("%include \"defines/elf.asm\"\n")
+        self._handle.write("bits 32\n") # For the beauty of the PoC
+
+    def _output_region_comments(self, region, comment="REGION"):
+        self._handle.write("; ---------------- %s ----------------\n"
+        "; Name: %s\n"
+        "; Base Address: %08X\n"
+        "; Size: %d bytes\n" % (comment, region.label, region.base_address, region.size))
+
+    def set_org(self, org):
+        self._handle.write("ORG 0x%08X\n" % (org))
+
+    def disassemble_elf32_ehdr_region(self, region, data):
+        self._output_region_comments(region, "ELF HEADER")
+        self._handle.write("%s:\n" % (region.label))
+        self._handle.write("\tistruc Elf32_Ehdr\n")
+        self._handle.write("\t\tat Elf32_Ehdr.e_ident,      db %d, `%s`, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n" % (data[0], data[1:4].decode(), data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_type,       dw %d\n" % (struct.unpack('<H', data[16:18])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_machine,    dw %d\n" % (struct.unpack('<H', data[18:20])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_version,    dd %d\n" % (struct.unpack('<L', data[20:24])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_entry,      dd 0x%08X\n" % (struct.unpack('<L', data[24:28])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_phoff,      dd %d\n" % (struct.unpack('<L', data[28:32])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_shoff,      dd %d\n" % (struct.unpack('<L', data[32:36])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_flags,      dd %d\n" % (struct.unpack('<L', data[36:40])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_ehsize,     dw %d\n" % (struct.unpack('<H', data[40:42])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_phentsize,  dw %d\n" % (struct.unpack('<H', data[42:44])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_phnum,      dw %d\n" % (struct.unpack('<H', data[44:46])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_shentsize,  dw %d\n" % (struct.unpack('<H', data[46:48])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_shnum,      dw %d\n" % (struct.unpack('<H', data[48:50])[0]))
+        self._handle.write("\t\tat Elf32_Ehdr.e_shstrndx,   dw %d\n" % (struct.unpack('<H', data[50:52])[0]))
+        self._handle.write("\tiend\n\n")
+
+
+    def disassemble_unknown_region(self, region, data):
+        self._output_region_comments(region, "UNKNOWN REGION")
+        self._handle.write("%s:\n" % (region.label))
+        self._handle.write("db 0x%02X" % (data[0]))
+        i = 1
+        while i < len(data):
+            if(i % 16 == 0):
+                self._handle.write("\ndb 0x%02X" % (data[i]))
+            else:
+                self._handle.write(", 0x%02X" % (data[i]))
+            i += 1
+        self._handle.write("\n")
+
+        self._handle.write("; ------------------------------------------------\n\n")
+
+    def disassemble_code_region(self, region, data):
+        self._output_region_comments(region, "EXECUTABLE CODE")
+        self._handle.write("%s:\n" % (region.label))
+        # TODO: check the target architecture.
+        md  = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
+        for instruction in md.disasm(data, region.base_address):
+            self._handle.write("\t%s %s\n" % (instruction.mnemonic, instruction.op_str))
+        #self.disassemble_unknown_region(region, data)
+
+    def disassemble_data_region(region, data):
+        # TODO
+        self.disassemble_unknown_region(region, data)
---
+++ okita/code_coverage.py
@@ -0,0 +1,78 @@
+class CodeCoverage(object):
+    def __init__(self, disassembler, regions, base_address=0x0):
+        """ Init a CodeCoverage class, which aims at "covering" a whole binary by
+        locating/differentiating code from (relevant) data, and so on."""
+        self._base_address = base_address
+        self._disassembler = disassembler
+        if regions is None:
+            self._regions = []
+        else:
+            self._regions = regions
+
+    @property
+    def base_address():
+        """ Retrieve the base memory address used by the code coverage """
+        return self._base_address
+
+    @property
+    def regions():
+        """ Retrieve the regions handled by the code coverage """
+        return self._regions
+
+    @property
+    def disassembler(self):
+        """ Get the current disassembler instance """
+        return self._disassembler
+
+    def disassemble(self, data):
+        self._disassembler.set_org(self._base_address)
+        offset = 0
+        for region in self._regions:
+            region.get_disassembled(self._disassembler, data[offset:offset+region.size])
+            offset += region.size
+
+class BinaryRegion(object):
+    def __init__(self, label, size, base_address, internal_symbols={}):
+        self._size = size
+        self._label = label
+        self._base_address = base_address
+        self._internal_symbols = internal_symbols
+    
+    @property
+    def size(self):
+        """ Retrieve the size of the region """
+        return self._size
+
+    @property
+    def label(self):
+        """ Retrieve the label of the region """
+        return self._label
+
+    @property
+    def base_address(self):
+        """ Retrieve the base address of the region """
+        return self._base_address
+
+    @property
+    def internal_symbols(self):
+        """ Retrieve some internal symbols of the region """
+        return self._internal_symbols
+
+    def get_disassembled(self, disassembler, data):
+        pass
+
+class UnknownRegion(BinaryRegion):
+    def get_disassembled(self, disassembler, data):
+        disassembler.disassemble_unknown_region(self, data)
+
+class CodeRegion(BinaryRegion):
+    def get_disassembled(self, disassembler, data):
+        disassembler.disassemble_code_region(self, data)
+
+class DataRegion(BinaryRegion):
+    def get_disassembled(self, disassembler, data):
+        disassembler.disassemble_data_region(self, data)
+
+class Elf32EhdrRegion(BinaryRegion):
+    def get_disassembled(self, disassembler, data):
+        disassembler.disassemble_elf32_ehdr_region(self, data)
