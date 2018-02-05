2cf1230361fe507cf31f94ba1a11b6e5e5a4361e
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-02-05 23:50:49 +0100

    Manual fixes

--- src/okita/base.py
+++ src/okita/base.py
@@ -1,3 +1,4 @@
 """Okita base utilities."""
+from enum import Enum
 
 Type = Enum('Type', 'PE ELF UNKNOWN')
--- src/okita/detect.py
+++ src/okita/detect.py
@@ -7,6 +7,7 @@ from .base import Type
 MAGIC_ELF = 0x464c457f  # 'ELF\x7F'
 MAGIC_MZ = 0x5a3D  # 'MZ\0\0'
 
+
 def detect(filename: str) -> Type:
     """Detect the binary type of `filename`.
     
@@ -21,8 +22,7 @@ def detect(filename: str) -> Type:
         magic_word = struct.unpack('<L', stream.read(4))[0]
         if magic_word == MAGIC_ELF:
             return Type.ELF
-        elif magic_word == MAGIC_PE:
+        elif magic_word == MAGIC_MZ:
             return Type.PE
         else:
-            return type.UNKNOWN
-        
+            return Type.UNKNOWN
