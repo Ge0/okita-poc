11c6a5b7f7ff52f71d87452852072f40b623d625
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-02-05 23:34:43 +0100

    Backup

---
+++ src/okita/detect.py
@@ -0,0 +1,13 @@
+"""Okita detection module."""
+
+
+def detect(filename: str):
+    """Detect the binary type of `filename`.
+    
+    Args:
+        filename: The filename of the binary to detect the type from.
+        
+    Returns:
+        The type of the binary.
+
+    """
